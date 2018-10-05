c8aef0e249256b69a136a40041206c6483dcadf2
darla <space-age-pete@users.noreply.github.com>
2018-10-04 22:11:20 -0500

    Updates

---
+++ precache-manifest.303f01ee48027b8dc9eb8e2279342747.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "17bac254e2f69149e83b",
+    "url": "/clicky-game/static/js/main.17bac254.chunk.js"
+  },
+  {
+    "revision": "dbef1f5110b91a0b2830",
+    "url": "/clicky-game/static/js/1.dbef1f51.chunk.js"
+  },
+  {
+    "revision": "17bac254e2f69149e83b",
+    "url": "/clicky-game/static/css/main.25857dc0.chunk.css"
+  },
+  {
+    "revision": "9d8afc2ae492482102ee3761854fbe53",
+    "url": "/clicky-game/index.html"
+  }
+];
